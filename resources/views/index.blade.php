e778033c5a83e79ea67c00c14dfb185fae556374
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-15 15:19:29 +0100

    Added placeholder for products.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -66,6 +66,205 @@
                     <input class="py-1 px-3 rounded-full w-full focus:ring-transparent" type="text" name="search" id="search" placeholder="Search products...">
                 </div>
             </nav>
+            <div class="relative bg-white overflow-hidden">
+                <div class="max-w-7xl mx-auto">
+                    <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                        <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+                            <polygon points="50,0 100,0 50,100 0,100" />
+                        </svg>
+
+                        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+                        </div>
+
+                        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                            <div class="sm:text-center lg:text-left">
+                                <h1 class="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
+                                    <span class="block">Shop from the</span>
+                                    <span class="block text-yellow-500">comfort of your home</span>
+                                </h1>
+                                <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                                </p>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+                <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.pexels.com/photos/3687999/pexels-photo-3687999.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </div>
+            </div>
+
+            <section class="text-gray-600 body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p class="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                                <p class="mt-1">$21.15</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                                <p class="mt-1">$12.00</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                                <p class="mt-1">$18.40</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p class="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                                <p class="mt-1">$21.15</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                                <p class="mt-1">$12.00</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268">
+                            </a>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                                <p class="mt-1">$18.40</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--<main class="p-5">
+                <div class="bg-white p-5 my-5 rounded shadow-sm">
+                    <h3 class="my-2 font-bold text-gray-800">Featured</h3>
+                    <hr>
+                    <div class="my-4 flex justify-between flex-wrap">
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white p-5 my-5 rounded shadow-sm">
+                    <h3 class="my-2 font-bold text-gray-800">Latest</h3>
+                    <hr>
+                    <div class="my-4 flex justify-between flex-wrap">
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                        <div class="p-3 border mb-5 md:mr-1 border-gray-300 hover:shadow-md w-full sm:w1/2 md:w-1/4 lg:w-1/6">
+                            <div class="h-52" style="background: url(https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500); background-size: cover; background-position: center;"></div>
+                            <div>
+                                <p>Can Coke</p>
+                                <p>NGN 100</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>-->
         </div>
     </body>
 </html>
