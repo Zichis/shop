80e51ed01a24313f2230308cb59de0ece76e9165
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-16 12:22:50 +0100

    Created dashboard component views.

---
+++ resources/views/layouts/dashboardHeader.blade.php
@@ -0,0 +1,5 @@
+<div class="px-10 py-5">
+    <p class="text-sm text-gray-500">{{ date('d F, Y') }}</p>
+    <h1 class="text-3xl">{{ auth()->user()->name }}</h1>
+    <p class="text-sm text-green-600 font-bold">Shop</p>
+</div>
