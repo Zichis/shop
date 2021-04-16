01f086a8187ce30048c8c942a78c1499965a2f7c
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-16 19:04:12 +0100

    Updated views Changed color.

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -1,9 +1,9 @@
 <div class="bg-gray-800 h-screen w-14 fixed text-center py-5">
     <div class="my-20 text-gray-200">
-        <a href="#" class="p-2 block my-5 hover:text-green-600 focus:outline-none">
+        <a href="{{ route('admin.dashboard') }}" class="p-2 block my-5 @if(Request::url() == route('admin.dashboard')) text-yellow-500 @endif hover:text-yellow-600 focus:outline-none">
             <i class="fas fa-th-large"></i>
         </a>
-        <a href="#" class="p-2 block my-5 hover:text-green-600 focus:outline-none">
+        <a href="#" class="p-2 block my-5 hover:text-yellow-600 focus:outline-none">
             <i class="fas fa-users"></i>
         </a>
     </div>
