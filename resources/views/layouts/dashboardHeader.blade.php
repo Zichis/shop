91c397293f8506d6546739f6077f2a856d7dad30
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-20 17:01:33 +0100

    Get full name in dashboard header.

--- resources/views/layouts/dashboardHeader.blade.php
+++ resources/views/layouts/dashboardHeader.blade.php
@@ -1,5 +1,5 @@
 <div class="px-10 py-5">
     <p class="text-sm text-gray-500">{{ date('d F, Y') }}</p>
-    <h1 class="text-3xl">{{ auth()->user()->name }}</h1>
+    <h1 class="text-3xl">{{ auth()->user()->fullName() }}</h1>
     <p class="text-sm text-yellow-600 font-bold">T & R Super Store</p>
 </div>
