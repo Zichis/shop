9545437ed16477861f705adb60429003eae87c69
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-20 10:29:44 +0100

    Displayed number of products in dashboard.

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+        $products = Product::all();
+
+        return view('admin.dashboard.index', [
+            'products' => $products
+        ]);
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -5,7 +5,7 @@
                 <div class="bg-white border-l-4 border-red-400 p-10 relative shadow-sm">
                     <i class="fas fa-cubes fa-3x absolute text-gray-100 right-5"></i>
                     <p class="text-gray-500">Products</p>
-                    <h3 class="text-gray-700 text-xl font-bold">3</h3>
+                    <h3 class="text-gray-700 text-xl font-bold">{{ count($products) }}</h3>
                 </div>
             </div>
             <div class="p-5 w-full md:w-1/2 lg:w-1/4">
