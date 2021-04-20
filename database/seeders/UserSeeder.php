0dd4941b9c0ac6a1520486bbc1bfdb67e52a1a54
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-20 12:23:29 +0100

    Updated user seeder with profile and roles.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Profile;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class UserSeeder extends Seeder
 {
@@ -15,9 +18,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $user = User::create([
             'email' => 'johndoe@company.com',
             'password' => Hash::make('password')
         ]);
+
+        $profile = Profile::create([
+            'user_id' => $user->id,
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'phone_number' => '08012345678',
+            'address' => '19 Crawford Avenue, Lagos'
+        ]);
+
+        $permissionsArray = ['add product', 'update product', 'delete product', 'view products'];
+        $admin = Role::create(['name' => 'ADMIN_ROLE']);
+
+        foreach ($permissionsArray as $permission) {
+            $admin->givePermissionTo(Permission::create(['name' => $permission]));
+        }
+
+        $user->assignRole($admin);
     }
 }
