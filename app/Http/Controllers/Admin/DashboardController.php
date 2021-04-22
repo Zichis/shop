d790134044bf290aa1be1bdf9852eb68d1a6a751
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-22 09:57:25 +0100

    Get users with customer role.

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        // TODO: Where quantity is more than 0
         $products = Product::all();
+        $customers = Role::findByName('ROLE_CUSTOMER')->users;
 
         return view('admin.dashboard.index', [
-            'products' => $products
+            'products' => $products,
+            'customers' => $customers
         ]);
     }
 }
