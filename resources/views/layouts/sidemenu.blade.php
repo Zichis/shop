fc5bd8f64a2f14f37589886a9b141fa47755c4ca
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-22 09:32:57 +0100

    Added users link to sidemenu.

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -6,7 +6,7 @@
         <a href="{{ route('admin.products.index') }}" class="p-2 block my-5 @if(Request::url() == route('admin.products.index') || Request::url() == route('admin.products.create')) text-yellow-500 @endif hover:text-yellow-600 focus:outline-none">
             <i class="fas fa-cubes"></i>
         </a>
-        <a href="#" class="p-2 block my-5 hover:text-yellow-600 focus:outline-none">
+        <a href="{{ route('admin.users.index') }}" class="p-2 block my-5 @if(Request::url() == route('admin.users.index')) text-yellow-500 @endif hover:text-yellow-600 focus:outline-none">
             <i class="fas fa-users"></i>
         </a>
     </div>
