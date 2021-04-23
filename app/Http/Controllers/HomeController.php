4c106e6d5d681d1cab69bf27ef1d0bb75bcbfa21
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-23 17:40:11 +0100

    Created home controller with index method.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::with(['images'])->get();
+
+        return view('index', [
+            'products' => $products
+        ]);
+    }
+}
