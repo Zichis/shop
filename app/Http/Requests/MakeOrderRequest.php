c05a28c1adc048f842c98d7a6e7d93e259ea3f7c
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-24 12:55:12 +0100

    Added validation to form request.

--- app/Http/Requests/MakeOrderRequest.php
+++ app/Http/Requests/MakeOrderRequest.php
@@ -13,7 +13,7 @@ class MakeOrderRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class MakeOrderRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'quantity' => ['required', 'numeric'],
+            'product_id' => ['required', 'numeric']
         ];
     }
 }
