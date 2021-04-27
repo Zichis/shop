fab9b1589e14fedf005ed6ccf21c2e9b142a5c1c
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-27 08:40:33 +0100

    Added link to product pictures and name.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -101,11 +101,13 @@
                     <div class="flex flex-wrap -m-4">
                         @foreach ($products as $product)
                             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                                <a class="block relative h-48 rounded overflow-hidden">
+                                <a href="{{ route('products.show', ['product' => $product->id]) }}" class="block relative h-48 rounded overflow-hidden">
                                     <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/products/' . $product->images[0]->name) }}">
                                 </a>
                                 <div class="mt-4">
-                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->name }}</h3>
+                                    <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                                        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->name }}</h3>
+                                    </a>
                                     <p class="mt-1">{{ $product->price }}</p>
                                 </div>
                             </div>
