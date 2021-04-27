db19fa8f77a8686ab4b5cfbb1d241464c06b9f6e
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-27 10:22:48 +0100

    Created cart controller with index.

---
+++ app/Http/Controllers/Customer/CartController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        $orders = Auth::user()->orders()->where('status', 'incomplete')->get();
+        $sum = Auth::user()->orders()->where('status', 'incomplete')->sum('total');
+
+        return view('customer.order.index', [
+            'orders' => $orders,
+            'total' => $sum
+        ]);
+    }
+}
