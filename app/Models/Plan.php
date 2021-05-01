756566bfe50006ceb77d38922abf67315d6a9b88
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-05-01 13:27:16 +0100

    Added fillable fields.

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Plan extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sub_name',
+        'name',
+        'single_price',
+        'single_discount',
+        'couple_price',
+        'couple_discount',
+        'family_price',
+        'family_discount'
+    ];
 }
