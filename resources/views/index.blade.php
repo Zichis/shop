72c36b3b678a2de1492ea526cc818024884a0d6c
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-05-03 15:10:33 +0100

    Added no items message.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,26 +28,30 @@
     </div>
 
     <section class="text-gray-600 body-font">
-        <div class="container px-5 py-14 mx-auto">
-            <p class="my-2">Latest Items</p>
-            <div class="flex flex-wrap -m-4">
-                @foreach ($products as $product)
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a href="{{ route('products.show', ['product' => $product->id]) }}" class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/products/' . $product->images[0]->name) }}">
-                        </a>
-                        <div class="mt-4">
-                            <a href="{{ route('products.show', ['product' => $product->id]) }}">
-                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->name }}</h3>
-                            </a>
-                            <p class="mt-1">{{ $product->price }}</p>
-                        </div>
+        @if (count($products) > 0)
+            <div class="container px-5 py-14 mx-auto">
+                    <p class="my-2">Latest Items</p>
+                    <div class="flex flex-wrap -m-4">
+                        @foreach ($products as $product)
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a href="{{ route('products.show', ['product' => $product->id]) }}" class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/products/' . $product->images[0]->name) }}">
+                                </a>
+                                <div class="mt-4">
+                                    <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                                        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->name }}</h3>
+                                    </a>
+                                    <p class="mt-1">{{ $product->price }}</p>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                @endforeach
             </div>
-        </div>
-        <div class="my-3 p-5 text-center">
-            <a href="{{ route('products.index') }}" class="p-3 border-2 border-yellow-600 font-bold text-yellow-600 hover:bg-yellow-600 hover:text-white">View more</a>
-        </div>
+            <div class="my-3 p-5 text-center">
+                <a href="{{ route('products.index') }}" class="p-3 border-2 border-yellow-600 font-bold text-yellow-600 hover:bg-yellow-600 hover:text-white">View more</a>
+            </div>
+        @else
+            <p class="m-5 text-center">No items</p>
+        @endif
     </section>
 </x-custom-layout>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,19 +2,23 @@
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-14 mx-auto">
             <div class="flex flex-wrap -m-4">
-                @foreach ($products as $product)
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a href="{{ route('products.show', ['product' => $product->id]) }}" class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/products/' . $product->images[0]->name) }}">
-                        </a>
-                        <div class="mt-4">
-                            <a href="{{ route('products.show', ['product' => $product->id]) }}">
-                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->name }}</h3>
+                @if (count($products) > 0)
+                    @foreach ($products as $product)
+                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a href="{{ route('products.show', ['product' => $product->id]) }}" class="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/products/' . $product->images[0]->name) }}">
                             </a>
-                            <p class="mt-1">&#8358; {{ $product->price }}</p>
+                            <div class="mt-4">
+                                <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->name }}</h3>
+                                </a>
+                                <p class="mt-1">&#8358; {{ $product->price }}</p>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                @else
+                    <p>No items</p>
+                @endif
             </div>
         </div>
     </section>
