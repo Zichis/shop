b448c2a1c27eb1c907fe84ac358f7be291b41715
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-05-03 17:35:00 +0100

    Created admin order page.

---
+++ resources/views/admin/order/index.blade.php
@@ -0,0 +1,35 @@
+<x-dashboard-layout>
+    <div class="p-5">
+        @if ($orders->isNotEmpty())
+            <div class="flex px-3 py-5 bg-yellow-600 text-white font-bold">
+                <div class="flex-1">Name</div>
+                <div class="flex-1">Unit Price</div>
+                <div class="flex-1">Quantity</div>
+                <div class="flex-1">Total</div>
+                <div class="flex-1">Actions</div>
+            </div>
+            @foreach ($orders as $order)
+                <div class="flex px-3 py-5 bg-yellow-50">
+                    <div class="flex-1">
+                        {{ $order->product_name }}
+                    </div>
+                    <div class="flex-1">
+                        &#8358;{{ $order->product_price }}
+                    </div>
+                    <div class="flex-1">
+                        {{ $order->quantity }}
+                    </div>
+                    <div class="flex-1">
+                        &#8358;{{ $order->total }}
+                    </div>
+                    <div class="flex-1">
+                        <a href="{{ route('admin.orders.confirm', ['order' => $order->id]) }}" class="py-1 px-3 bg-green-500 text-white rounded hover:bg-green-600">Confirm</a>&nbsp;
+                        <a href="{{ route('admin.orders.reject', ['order' => $order->id]) }}" class="py-1 px-3 bg-red-500 text-white rounded hover:bg-red-600">Reject</a>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            <p class="p-5">No pending orders.</p>
+        @endif
+    </div>
+</x-dashboard-layout>
