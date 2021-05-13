3fb0755816a00fe713f85d49589c460e3ab10cab
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-05-13 01:12:07 +0100

    Made sidemenu scrollable.

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-gray-800 h-screen w-14 fixed text-center py-5">
+<div class="bg-gray-800 h-screen w-14 fixed text-center py-5 overflow-y-scroll">
     <div class="my-20 text-gray-200">
         <a href="{{ route('admin.dashboard') }}" class="p-2 block my-5 @if(Request::url() == route('admin.dashboard')) text-yellow-500 @endif hover:text-yellow-600 focus:outline-none">
             <i class="fas fa-th-large"></i>
